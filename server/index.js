d145bf02a396d7de4396a7868ac310f5d9c72178
kavish <boyalkavish@gmail.com>
2022-07-15 13:16:14 +0530

    Setup

--- server/index.js
+++ server/index.js
@@ -9,3 +9,12 @@ app.use(bodyParser.json({ limit: "30mb", extended:true}));
 app.use(bodyParser.urlencoded({ limit:"30mb", extended:true}));
 
 app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://kavish:kavish1999@cluster0.fem4iwj.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', False);
