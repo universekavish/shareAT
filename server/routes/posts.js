00d5af875968c7226c0440b25a4597bdfe9642e9
kavish <boyalkavish@gmail.com>
2022-07-15 15:33:58 +0530

    creating model and adding route logics

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 // Here we won't add logic, if too many logics then we can be lost between all the logics and all the requests, so routes logic is separately written in controllers/posts.js.
 import express from "express";
 
-import { getPosts } from '../controllers/posts.js';
+import { getPosts, createPost } from '../controllers/posts.js';
 const router = express.Router();
 
 // http://localhost:5000/posts
 router.get('/', getPosts);
+router.post('/', createPost);
 
 export default router;
