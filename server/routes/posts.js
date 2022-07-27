647354588afed947df9e99236beab9749c2c728e
kavish <boyalkavish@gmail.com>
2022-07-27 15:07:09 +0530

    commit

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,7 @@
 // Here we won't add logic, if too many logics then we can be lost between all the logics and all the requests, so routes logic is separately written in controllers/posts.js.
 import express from "express";
 
-import { getPosts, createPost, updatePost, deletePost } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
 const router = express.Router();
 
 // http://localhost:5000/posts
@@ -9,5 +9,6 @@ router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
 router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
 export default router;
