5c4e554dd68f1ccae798f6c3b250b22cdf2aa822
kavish <boyalkavish@gmail.com>
2022-07-31 21:52:53 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-import shareAT from './images/shareAT.png';
+import React from 'react';
+import { Container, Grow, Grid} from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { getPosts } from './actions/posts';
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import useStyles from './styles';
+import NavBar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
 
-const App = () => {
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-
-    // successful dispatch
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
-
-    return (
+const App = () => (
+    <BrowserRouter>
         <Container maxWidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">ShareAT</Typography>
-                <img className={classes.image} src={shareAT} alt="shareAT" height="60"/>
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId} />
-                        </Grid>
-                    </Grid>
-                </Container>
-
-            </Grow>
+            <NavBar />
+            <Home />
         </Container>
-    );
-}
+    </BrowserRouter>
+);
 
 export default App;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import { AppBar, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
+
+import useStyles from './styles';
+import shareAT from '../../images/shareAT.png';
 
 const NavBar = () => {
     const classes = useStyles();
 
+    const user = null;
+
     return (
     <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">ShareAT</Typography>
-        <img className={classes.image} src={shareAT} alt="shareAT" height="60"/>
+        <div className={classes.brandContainer}>
+            <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">ShareAT</Typography>
+            <img className={classes.image} src={shareAT} alt="shareAT" height="60"/>
+        </div>
+        <Toolbar className={classes.toolbar}>
+            {user ? (
+                <div className={classes.profile}>
+                    <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+                    <Typography className={classes.userName} varian="h6">{user.result.name}</Typography>
+                    <Button variant="contained" className={classes.logout} color="secondary">LogOut</Button>
+                </div>
+            ) : (
+                <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+            )}
+        </Toolbar>
     </AppBar>
-    )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
