0862f6b3af9b84d3f913b56a1fbe7597b30cc627
kavish <boyalkavish@gmail.com>
2022-08-03 10:03:40 +0530

    commit

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-
 import useStyles from './styles';
+import Input from './Input';
 
 const Auth = () => {
     const classes = useStyles();
+    const [showPassword, setShowPassword] = useState();
 
     const isSignUp = false;
 
@@ -26,18 +27,14 @@ const Auth = () => {
                 <Typography variant="h5">{ isSignUp ? "Sign Up" : "Sign In"}</Typography>
                 <Form className={classes.form} onSubmit={handleSubmit}>
                     <Grid container spacing={2}>
-                        {
-                            isSignUp && (
+                        { isSignUp && (
                                 <>
-                                    <Grid xs={6} md={12}>
-                                        <TextField name="firstName" label="First Name" handleChange={handleChange} autoFocus xs={6} />
-                                    </Grid>
-                                    <Grid xs={6} md={12}>
-                                        <TextField name="firstName" label="First Name" handleChange={handleChange} autoFocus xs={6} />
-                                    </Grid>
+                                    <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
+                                    <Input name="lastName" label="Last Name" handleChange={handleChange} half />
                                 </>
-                            )
-                        }
+                            )}
+                            <Input name="email" label="Email Address" handleChange={handleChange} type="email"/>
+                            <Input name="password" label="password" handleChange={handleChange} type="password"/>
                     </Grid>
                 </Form>
             </Paper>
